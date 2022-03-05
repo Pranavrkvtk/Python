4c4763cbe51c3d280b35d153155bca84a6a6cc8c
Pranavrkvtk <94657009+Pranavrkvtk@users.noreply.github.com>
2022-03-05 12:05:43 +0530

    co3

--- co3/co3-2.py
+++ co3/co3-2.py
@@ -1,3 +1,8 @@
+# Create a package graphics with modules rectangle, circle and sub-package 3D-graphics with
+# modules cuboid and sphere. Include methods to find area and perimeter of respective figures
+# in each module. Write programs that finds area and perimeter of figures by different importing
+# statements. (Include selective import of modules and import * statements)
+
 import graphics.trdGraphics.cuboid as cube
 import graphics.trdGraphics.shpere as sp
 import graphics.circle as ci
